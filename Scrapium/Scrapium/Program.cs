d7c585e02e08aaebdc7374b2fa7671bcef86ca9b
Santiago <sstriatlon@gmail.com>
2017-08-10 21:58:42 -0300

    Initial commit

---
+++ Scrapium/Scrapium/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace Scrapium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var html = @"https://www.cronista.com/";
+
+            HtmlWeb web = new HtmlWeb();
+
+            var htmlDoc = web.Load(html);
+
+            var node = htmlDoc.DocumentNode.SelectSingleNode("//head/title");
+
+            Console.WriteLine("Node Name: " + node.Name + "\n" + node.OuterHtml);
+
+            Console.ReadLine();
+        }
+    }
+}
