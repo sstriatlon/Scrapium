da3b01e10c25dcc79628014225c02b82b54359fc
Santiago <sstriatlon@gmail.com>
2017-08-11 00:16:17 -0300

    Libraries adding..

--- Scrapium/Scrapium/Program.cs
+++ Scrapium/Scrapium/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using HtmlAgilityPack;
+using YahooFinanceApi;
+using System.Threading.Tasks;
 
 namespace Scrapium
 {
@@ -7,17 +9,31 @@ namespace Scrapium
     {
         static void Main(string[] args)
         {
-            var html = @"https://www.cronista.com/";
+            //var html = @"https://www.cronista.com/";
 
-            HtmlWeb web = new HtmlWeb();
+            //HtmlWeb web = new HtmlWeb();
 
-            var htmlDoc = web.Load(html);
+            //var htmlDoc = web.Load(html);
 
-            var node = htmlDoc.DocumentNode.SelectSingleNode("//head/title");
+            //var node = htmlDoc.DocumentNode.SelectSingleNode("//head/title");
 
-            Console.WriteLine("Node Name: " + node.Name + "\n" + node.OuterHtml);
+            //Console.WriteLine("Node Name: " + node.Name + "\n" + node.OuterHtml);
 
+
+            Req();
+
+            
             Console.ReadLine();
+        }
+
+        static async Task<string> Req()
+        {
+            var quotes = await Yahoo.Symbol("AAPL", "GOOG").Tag(Tag.LastTradePriceOnly, Tag.Open, Tag.DaysHigh, Tag.DaysLow, Tag.PreviousClose).GetAsync();
+            var aapl = quotes["AAPL"];
+            //var price = aapl[Tag.LastTradePriceOnly];
+
+            Console.WriteLine(aapl);
+            return aapl.ToString();
         }
     }
 }
